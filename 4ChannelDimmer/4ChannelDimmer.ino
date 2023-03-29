7686c6a672b7f47d277d6a0976ae4f9ebe40b945
ToniKousek <toni.kousek@gmail.com>
2023-03-29 16:17:59 +0200

    beautified code

--- 4ChannelDimmer/4ChannelDimmer.ino
+++ 4ChannelDimmer/4ChannelDimmer.ino
@@ -7,23 +7,38 @@
     connect 4 wires from the ch1-ch4 of the circuit to arduino pins     ch? -> D?  * 4
 */
 
-const byte POTENTIOIMETER_PINS[] = {A1, A2, A3, A4}; // numbers and amount of potentiometers changeable 
-unsigned short potentiometer_values[4];
-
-const byte DIMMER_PINS[] = {6,9,10,11}; //numbers and amount of channels changeable 
-dimmerLamp dimmers[] = {dimmerLamp(DIMMER_PINS[0]), dimmerLamp(DIMMER_PINS[1]), dimmerLamp(DIMMER_PINS[2]), dimmerLamp(DIMMER_PINS[3])}; // numbers and amount of lamps changeable 
+const byte POTENTIOIMETER_PINS[] = {A1, A2, A3, A4}; // numbers and amount of potentiometers changeable
+const byte DIMMER_PINS[] = {6, 9, 10, 11};           // numbers and amount of channels changeable
 
 const byte NUM_OF_LIGHTS = 4; // changeable
-const byte MIN_LIGHT = 55; // the minimum percentage of power that goes to the lamps    changeable
-const byte MAX_LIGHT = 80; // the maximum percentage of power that goes to the lamps    changeable
+const byte MIN_LIGHT = 55;    // the minimum percentage of power that goes to the lamps    changeable
+const byte MAX_LIGHT = 80;    // the maximum percentage of power that goes to the lamps    changeable
+
+dimmerLamp dimmers[] = {dimmerLamp(DIMMER_PINS[0]), dimmerLamp(DIMMER_PINS[1]), dimmerLamp(DIMMER_PINS[2]), dimmerLamp(DIMMER_PINS[3])}; // numbers and amount of lamps changeable
+double values[4];                                                                                                                        // percentage of power [0,100]
 
 // the builtin map() function does not return a float, so this function is the same as the map() however it returns a double
-double double_map(long x, long in_min, long in_max, long out_min, long out_max) {
-  return (x - in_min) * (out_max - out_min) / (double)(in_max - in_min) + out_min;
+double double_map(long x, long in_min, long in_max, long out_min, long out_max)
+{
+    return (x - in_min) * (out_max - out_min) / (double)(in_max - in_min) + out_min;
+}
+
+// promijeni ovu funkciju u svoju
+void get_values()
+{
+    for (byte i = 0; i < NUM_OF_LIGHTS; i++)
+    {
+        values[i] = double_map(analogRead(POTENTIOIMETER_PINS[i]), 1, 1024, MAX_LIGHT, MIN_LIGHT);
+
+        // debugging
+        Serial.print(double_map(analogRead(POTENTIOIMETER_PINS[i]), 1, 1024, MAX_LIGHT, MIN_LIGHT));
+        Serial.print("\t");
+    }
+    Serial.println();
 }
 
 void setup()
-    {
+{
     // debugging
     Serial.begin(9600);
 
@@ -31,28 +46,21 @@ void setup()
     {
         // setup dimmers
         dimmers[i].begin(NORMAL_MODE, ON);
+        pinMode(DIMMER_PINS[i], OUTPUT);
+
         // setup potentiometer pins
         pinMode(POTENTIOIMETER_PINS[i], INPUT);
     }
-    pinMode(6,OUTPUT);
-    pinMode(9,OUTPUT);
-    pinMode(10,OUTPUT);
-    pinMode(11,OUTPUT);
 }
 
 void loop()
 {
     // get the values
-    for (byte i = 0; i < NUM_OF_LIGHTS; i++)
-    {
-        potentiometer_values[i] = double_map(analogRead(POTENTIOIMETER_PINS[i]), 1, 1024, MAX_LIGHT, MIN_LIGHT);
-        Serial.print(double_map(analogRead(POTENTIOIMETER_PINS[i]), 1, 1024, MAX_LIGHT, MIN_LIGHT));
-        Serial.print("\t");
-    }
-    Serial.println();
+    get_values();
+
     // set the values
     for (byte i = 0; i < NUM_OF_LIGHTS; i++)
     {
-        dimmers[i].setPower(potentiometer_values[i]);
+        dimmers[i].setPower(values[i]);
     }
 }
