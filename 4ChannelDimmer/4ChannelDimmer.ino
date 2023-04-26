012387e64ead212651c22a27b3ee2735a8b01937
ToniKousek <toni.kousek@gmail.com>
2023-04-26 15:43:36 +0200

    Changed for esp32

--- 4ChannelDimmer/4ChannelDimmer.ino
+++ 4ChannelDimmer/4ChannelDimmer.ino
@@ -2,20 +2,26 @@
 
 /*
     ? == you can choose the number
-    connect the sync pin of the circuit to the arduino's D2 pin         sync -> D2
-    connect 4 wires from potentiometer to arduino pins                  potentiometer -> D?  * 4
-    connect 4 wires from the ch1-ch4 of the circuit to arduino pins     ch? -> D?  * 4
+    connect the sync pin of the circuit to the esp32's GPIO pin       sync -> GPIO?
+    connect 4 wires from potentiometer to esp32 pins                  potentiometer -> GPIO?  * 4
+    connect 4 wires from the ch1-ch4 of the circuit to esp32 pins     ch? -> GPIO?  * 4
 */
 
-const byte POTENTIOIMETER_PINS[] = {A1, A2, A3, A4}; // numbers and amount of potentiometers changeable
-const byte DIMMER_PINS[] = {6, 9, 10, 11};           // numbers and amount of channels changeable
+const byte POTENTIOIMETER_PINS[] = {1, 2, 3, 4}; // numbers and amount of potentiometers changeable
+const byte DIMMER_PINS[] = {6, 9, 10, 11};       // numbers and amount of channels changeable
+const byte INTERRUPT_PIN = 12;
 
 const byte NUM_OF_LIGHTS = 4; // changeable
 const byte MIN_LIGHT = 55;    // the minimum percentage of power that goes to the lamps    changeable
 const byte MAX_LIGHT = 80;    // the maximum percentage of power that goes to the lamps    changeable
 
-dimmerLamp dimmers[] = {dimmerLamp(DIMMER_PINS[0]), dimmerLamp(DIMMER_PINS[1]), dimmerLamp(DIMMER_PINS[2]), dimmerLamp(DIMMER_PINS[3])}; // numbers and amount of lamps changeable
-double values[4];                                                                                                                        // percentage of power [0,100]
+dimmerLamp dimmers[] = {
+    dimmerLamp(DIMMER_PINS[0], INTERRUPT_PIN),
+    dimmerLamp(DIMMER_PINS[1], INTERRUPT_PIN),
+    dimmerLamp(DIMMER_PINS[2], INTERRUPT_PIN),
+    dimmerLamp(DIMMER_PINS[3], INTERRUPT_PIN)}; // numbers and amount of lamps changeable
+
+double values[4]; // percentage of power [0,100]
 
 // the builtin map() function does not return a float, so this function is the same as the map() however it returns a double
 double double_map(long x, long in_min, long in_max, long out_min, long out_max)
@@ -31,8 +37,12 @@ void get_values()
         values[i] = double_map(analogRead(POTENTIOIMETER_PINS[i]), 1, 1024, MAX_LIGHT, MIN_LIGHT);
 
         // debugging
+        Serial.print(analogRead(POTENTIOIMETER_PINS[i]));
+        Serial.print(" = ");
         Serial.print(double_map(analogRead(POTENTIOIMETER_PINS[i]), 1, 1024, MAX_LIGHT, MIN_LIGHT));
-        Serial.print("\t");
+        Serial.print(" is ");
+        Serial.print(dimmers[i].getPower());
+        Serial.println();
     }
     Serial.println();
 }
